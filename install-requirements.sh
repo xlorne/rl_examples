c67c7dfbe539e1c56a6f8491ef98bdf94e6a1aa5
xlorne <1991wangliang@gmail.com>
2023-08-09 11:19:36 +0800

    add proxy

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,5 +1,10 @@
 conda create -n rl_examples python=3.8 -y
 conda activate rl_examples
 
+# CPU install
+pip3 install torch torchvision torchaudio --proxy=http://127.0.0.1:7890
+
+# GPU cuda11.08 install
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install -r requirements.txt
+
+pip3 install -r requirements.txt --proxy=http://127.0.0.1:7890
